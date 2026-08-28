3d140faa9732bea977f846fc0b5ef9fc37b803fd
TibTIBUS <marie.thibaut2105@gmail.com>
2026-08-28 14:06:43 +0200

    Corriger la génération de la page d'accueil pour GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,8 @@
 import type { NextConfig } from "next";
 
-const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
-const githubBasePath = "/BACHELET-PM";
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  ...(isGitHubPages
-    ? {
-        basePath: githubBasePath,
-        assetPrefix: githubBasePath,
-      }
-    : {}),
 };
 
 export default nextConfig;
